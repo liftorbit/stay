5e30b092cb63331e3b8178260726ee0b43bcf009
Jaedson Silva <contacanal798@gmail.com>
2023-02-13 20:49:51 -0300

    feat: start RCS system and wait a connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <Arduino.h>
+#include "logging.h"
+#include "rcs.h"
+
+#define BUILTIN_LED 2
+
+RCS rcs;
+
 
 void setup() {
     Serial.begin(9600);
-    Serial.println("STAY B Startup");
+
+    pinMode(BUILTIN_LED, OUTPUT);
+
+    log("info", "STAY B Startup");
+
+    rcs.begin();
+    log("info", "RCS started");
+    log("wait", "Wait RCS connection...");
+
+    while (!rcs.hasConnected()) {
+        for(int i = 0; i < 4; i++) {
+            digitalWrite(BUILTIN_LED, HIGH);
+            delay(80);
+            digitalWrite(BUILTIN_LED, LOW);
+            delay(80);
+        }
+    }
+
+    rcs.send("connected");
 };
 
 void loop() {
