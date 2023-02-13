1c5d89571fc2dadaa2efc529ba0fdfd19429bdb4
Jaedson Silva <contacanal798@gmail.com>
2023-02-13 20:40:56 -0300

    feat: log data in serial communication

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,12 +1,6 @@
 #include <Arduino.h>
 #include "logging.h"
-#include "rcs.h"
-
-RCS rcs;
 
 void log(String type, String message) {
-    rcs.begin();
-    
-    String logMsg = "log:" + type + "," + message;
-    rcs.send(logMsg);
+    Serial.println(type + ": " + message);
 };
