c26bdd83bf331e21af710bdb2d64d1d62af7ed13
Jaedson Silva <contacanal798@gmail.com>
2023-05-18 14:49:14 -0300

    feat: create functions to send telemetry data

--- src/telemetry.cpp
+++ src/telemetry.cpp
@@ -22,3 +22,41 @@ bool Telemetry::dataAvailable() {
 String Telemetry::receive() {
     return ConnSerial.readString();
 };
+
+void Telemetry::telemetry(bool engine, float temp, float alt, float ps, float acce, float lat, float lon) {
+    String sEng = String(engine);
+    String sTemp = String(temp);
+    String sAlt = String(alt);
+    String sPs = String(ps);
+    String sAcce = String(acce);
+    String sLat = String(lat);
+    String sLon = String(lon);
+
+    String dataList[7] = {sEng, sTemp, sAlt, sPs, sAcce, sLat, sLon};
+    String fullData = "";
+
+    for(int i = 0; i < 7; i++) {
+        fullData.concat(dataList[i]);
+        fullData.concat(',');
+    }
+
+    ConnSerial.println(fullData);
+};
+
+void Telemetry::telemetry(bool engine, float temp, float alt, float ps, float acce) {
+    String sEng = String(engine);
+    String sTemp = String(temp);
+    String sAlt = String(alt);
+    String sPs = String(ps);
+    String sAcce = String(acce);
+
+    String dataList[5] = {sEng, sTemp, sAlt, sPs, sAcce};
+    String fullData = "";
+
+    for(int i = 0; i < 5; i++) {
+        fullData.concat(dataList[i]);
+        fullData.concat(',');
+    }
+
+    ConnSerial.println(fullData);
+};
