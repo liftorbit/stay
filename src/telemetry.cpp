2e6467b3a2b9fcc42eae000c36d0b9bf82659790
Jaedson Silva <contacanal798@gmail.com>
2023-05-18 14:28:38 -0300

    feat: create Telemetry::begin() function

---
+++ src/telemetry.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include "telemetry.h"
+
+const int serialRX = 16;
+const int serialTX = 17;
+
+HardwareSerial ConnSerial(1);
+
+void Telemetry::begin() {
+    ConnSerial.begin(9600);
+};
