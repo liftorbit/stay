301c53ce86bf16539aaccffe49001bdd51000a93
Jaedson Silva <jaedson.dev@proton.me>
2023-05-24 11:23:13 -0300

    feat: create GPS::update function

--- src/gps.cpp
+++ src/gps.cpp
@@ -1,13 +1,24 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
+#include <TinyGPSPlus.h>
 #include "gps.h"
 
 HardwareSerial GPSSerial(1);
+TinyGPSPlus gps;
 
 void GPS::begin() {
     GPSSerial.begin(9600, SERIAL_8N1, 36, 34);
 
     while(!GPSSerial.available()) {
         delay(100);
+    }
+};
+
+void GPS::update() {
+    while(GPSSerial.available()) {
+        if(gps.encode(GPSSerial.read())) {
+            this->lon = gps.location.lng();
+            this->lat = gps.location.lat();
+        }
     }
 };
