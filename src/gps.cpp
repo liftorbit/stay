1a594bc5f150ef169fa27507b711c90e0ff4d443
Jaedson Silva <jaedson.dev@proton.me>
2023-05-28 19:51:24 -0300

    feat: use pins 4 and 15 to GPS

--- src/gps.cpp
+++ src/gps.cpp
@@ -4,10 +4,10 @@
 #include "gps.h"
 
 HardwareSerial GPSSerial(1);
-TinyGPSPlus gps;
+TinyGPSPlus tinyGPS;
 
 void GPS::begin() {
-    GPSSerial.begin(9600, SERIAL_8N1, 36, 34);
+    GPSSerial.begin(9600, SERIAL_8N1, 4, 15);
 
     while(!GPSSerial.available()) {
         delay(100);
@@ -16,9 +16,9 @@ void GPS::begin() {
 
 void GPS::update() {
     while(GPSSerial.available()) {
-        if(gps.encode(GPSSerial.read())) {
-            this->lon = gps.location.lng();
-            this->lat = gps.location.lat();
+        if(tinyGPS.encode(GPSSerial.read())) {
+            this->lon = tinyGPS.location.lng();
+            this->lat = tinyGPS.location.lat();
         }
     }
 };
