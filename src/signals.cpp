98c992824c6654a47f7d0384420d37193a79c5a3
Jaedson Silva <jaedson.dev@proton.me>
2023-09-10 14:39:59 -0300

    feat: create waitBCS() signal

--- src/signals.cpp
+++ src/signals.cpp
@@ -9,3 +9,20 @@ Signals::Signals(int buzzerPin, int ledPin) {
     this->buzzerPin = buzzerPin;
     this->ledPin = ledPin;
 }
+
+void Signals::waitBCS() {
+    ledcWriteTone(0, 500);
+    delay(100);
+    ledcWriteTone(0, 700);
+    delay(100);
+    ledcWriteTone(0, 900);
+    delay(100);
+    ledcWrite(0, 0);
+
+    for(int i = 0; i < 3; i++) {
+        digitalWrite(this->ledPin, HIGH);
+        delay(200);
+        digitalWrite(this->ledPin, LOW);
+        delay(200);
+    }
+}
